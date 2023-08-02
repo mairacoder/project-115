af2cc3a62f3adc4fd6984cb960b9441c30674e95
mairacoder <125368352+mairacoder@users.noreply.github.com>
2023-08-02 16:46:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function preload() {
+mustache = loadImage('mustachi.png')
 }
 
 function setup(){
@@ -13,12 +14,17 @@ function setup(){
     poseNet.on('pose', gotPoses)
 }
 
+noseX=0;
+noseY=0;
+
 function draw(){
   image(video, 0, 0, 300, 300);
+  image(mustache, noseX - 15, noseY , 40, 30)
 }
 
 function take_snapshot(){
     save('myFilterImage.png')
+  
 }
 
 
@@ -33,8 +39,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
 }
-
